ac312c63410f0edc5d76f67350f26168f8c6f2a3
tfermoso <tomas.fermoso@gmail.com>
2026-01-13 20:22:36 +0100

    home con usuario y logout

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -39,8 +39,16 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+            <!-- Si estoy logeado email y enlace a logout, sino nada -->
+            <?php if ($this->request->getAttribute('identity')): ?>
+                <span><?= $this->request->getAttribute('identity')->email ?></span>
+                <?= $this->Html->link(
+                    'Logout',
+                    ['controller' => 'Users', 'action' => 'logout'],
+                    ['class' => 'button button-outline']
+                ) ?>
+            <?php endif; ?>
+
         </div>
     </nav>
     <main class="main">
